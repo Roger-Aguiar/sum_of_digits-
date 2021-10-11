c0d8e6bd84763224a2c1c33ee70b2bea4501556b
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-10-11 09:43:30 -0300

    It creates the user prompt

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { SumOfDigits } from "./digital.root";
+import readline = require('readline');
+
+let userInput = readline.createInterface({input: process.stdin, output: process.stdout});
+
+userInput.question("Please, enter an integer number: ", 
+function(answer)
+{
+    let integerNumber = new SumOfDigits(answer);
+    let digitalRootSum = integerNumber.digitalRoot();
+    console.log(`The digital root sum is: ${digitalRootSum}`);
+});
